abdb90f1f8f8b8bbd09df4c351b825495203d776
suegoiee <suegoieenae@hotmail.com.tw>
2023-06-30 23:27:35 +0800

    store image directly from link

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/aws/aws-sdk-go/aws"
@@ -19,7 +18,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/joho/godotenv"
-	"github.com/sclevine/agouti"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -60,35 +58,6 @@ var (
 )
 
 func init() {
-	link1 := "https://tw.stock.yahoo.com/news/%E6%95%99%E5%AD%B8-%E5%85%A8%E6%94%AF%E4%BB%98%E6%98%AF%E4%BB%80%E9%BA%BC-%E5%A6%82%E4%BD%95%E8%A8%BB%E5%86%8A%E7%B6%81%E5%AE%9A-2022%E5%84%AA%E6%83%A0%E6%B4%BB%E5%8B%95-%E7%B6%81%E9%8A%80%E8%A1%8C%E5%B8%B3%E6%88%B6-041900616.html"
-
-	driver := agouti.ChromeDriver()
-
-	if errs := driver.Start(); errs != nil {
-		log.Fatal("Failed to start ChromeDriver:", errs)
-	}
-	defer driver.Stop()
-
-	page, errs := driver.NewPage(agouti.Browser("chrome"))
-	if errs != nil {
-		log.Fatal("Failed to open page:", errs)
-	}
-
-	if errs := page.Navigate(link1); errs != nil {
-		log.Fatal("Failed to navigate:", errs)
-	}
-
-	time.Sleep(5 * time.Second) // Give enough time for the page to load and images to render
-
-	imgSrcs, errs := page.All("div.caas-body img").Attribute("src")
-	if errs != nil {
-		log.Fatal("Failed to retrieve image source URLs:", errs)
-	}
-
-	for _, imgSrc := range imgSrcs {
-		fmt.Println(imgSrc)
-	}
-
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("無法讀取 .env 檔案:", err)
@@ -125,7 +94,6 @@ func init() {
 		SecretAccessKey: os.Getenv("S3SECRETACCESSKEY"),
 	}
 
-	// 建立新的 AWS 會話
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String(s3Config.Region),
 		Credentials: credentials.NewStaticCredentials(s3Config.AccessKeyID, s3Config.SecretAccessKey, ""),
@@ -134,28 +102,23 @@ func init() {
 		log.Fatal("Failed to create AWS session", err)
 	}
 
-	// 建立 S3 服務的客戶端
 	svc = s3.New(sess)
 }
 
 func main() {
-	// 要爬取的網頁 URL
 	url := "https://tw.stock.yahoo.com/news/"
 
-	// 發送 HTTP 請求並獲取響應
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 
-	// 使用 goquery 解析 HTML
 	doc1, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 提取標題並輸出
 	results := make(map[string]bool)
 	doc1.Find("a").Each(func(i int, s *goquery.Selection) {
 		href, exists := s.Attr("href")
@@ -203,42 +166,61 @@ func main() {
 			nextDoc.Find("div.caas-body img").Each(func(i int, s *goquery.Selection) {
 				imgSrc, exists := s.Attr("src")
 				if !exists {
-					return
+					imgSrc, exists = s.Attr("data-src")
+					if !exists {
+						return
+					}
 				}
 
 				s.AppendHtml("<p>{{img" + strconv.Itoa(i) + "}}<p>")
-				// 下載圖片
-				downloadedImage, err := downloadImage(imgSrc, imageDir)
+				// Download the image from the URL
+				resp, err := http.Get(imgSrc)
 				if err != nil {
-					log.Printf("Failed to download image %s: %s", imgSrc, err)
+					fmt.Println("Failed to download image:", err)
+					return
+				}
+				defer resp.Body.Close()
+
+				contentType := resp.Header.Get("Content-Type")
+				fileExtension := mimeToExtension(contentType)
+
+				// Get the filename from the URL
+				filename := ""
+				fileKey := ""
+				if strings.LastIndex(imgSrc, "/") != -1 {
+					filename = imgSrc[strings.LastIndex(imgSrc, "/")+1:] + fileExtension
+					fileKey = "images/" + filename
 				}
 
-				pwd, _ := os.Getwd()
-				filePath := pwd + "\\files\\download\\" + downloadedImage
-				fileKey := "images/" + downloadedImage
-				// 打開欲上傳的圖片檔案
-				file, err := os.Open(filePath)
-				if err != nil {
-					log.Fatal("Failed to open file", err)
-				}
-				defer file.Close()
-
-				// 設定上傳參數
-				params := &s3.PutObjectInput{
+				// Create a new S3 object
+				object := &s3.PutObjectInput{
 					Bucket: aws.String(s3Config.Bucket),
 					Key:    aws.String(fileKey),
-					Body:   file,
+				}
+				buffer := bytes.NewBuffer(nil)
+
+				// Copy the response body into the buffer
+				_, err = io.Copy(buffer, resp.Body)
+				if err != nil {
+					fmt.Println("Failed to copy image data to buffer:", err)
+					return
 				}
 
-				// 上傳圖片至 S3
-				_, err = svc.PutObject(params)
+				// Set the object's body to the buffer
+				object.Body = bytes.NewReader(buffer.Bytes())
+
+				// Upload the image to S3
+				_, err = svc.PutObject(object)
 				if err != nil {
-					log.Fatal("Failed to upload file to S3", err)
+					fmt.Println("Failed to upload image to S3:", err)
+					return
 				}
 
 				// 取得公開連結
 				publicURL := fmt.Sprintf("https://%s.s3-%s.amazonaws.com/%s", s3Config.Bucket, s3Config.Region, fileKey)
-				description := s.ParentsUntil("figure.caas-figure").Find("figcaption.caption-collapse").Text()
+				figcaption := s.Closest("figure").Find("figcaption")
+				description := figcaption.Text()
+				figcaption.Remove()
 				newImage := NewsImage{
 					Link:        publicURL,
 					Description: description,
@@ -263,67 +245,7 @@ func main() {
 	}
 
 	insertedCount := len(result.InsertedIDs)
-
-	deleteError := deleteFilesInFolder(imageDir)
-	if deleteError != nil {
-		fmt.Printf("Error deleting files: %v", err)
-		return
-	}
-
 	fmt.Println("Inserted count:", insertedCount)
-}
-
-// 下載圖片
-func downloadImage(url string, directory string) (string, error) {
-	var filePath string
-	response, err := http.Get(url)
-	if err != nil {
-		return filePath, err
-	}
-	defer response.Body.Close()
-
-	contentType := response.Header.Get("Content-Type")
-	fileExtension := mimeToExtension(contentType)
-
-	// 確定響應狀態碼為200 OK
-	if response.StatusCode != http.StatusOK {
-		return filePath, fmt.Errorf("received non-200 status code: %d", response.StatusCode)
-	}
-
-	// 從URL中提取圖片文件名
-	fileName := filepath.Base(url) + fileExtension
-	filePath = filepath.Join(directory, fileName)
-
-	// 建立文件
-	file, err := os.Create(filePath)
-	if err != nil {
-		return filePath, err
-	}
-	defer file.Close()
-
-	// 將圖片寫入文件
-	_, err = io.Copy(file, response.Body)
-	if err != nil {
-		return filePath, err
-	}
-
-	return fileName, err
-}
-
-func deleteFilesInFolder(folderPath string) error {
-	files, err := os.ReadDir(folderPath)
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		err = os.RemoveAll(folderPath + "/" + file.Name())
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func mimeToExtension(contentType string) string {
