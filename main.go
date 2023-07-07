9624f905634e29c73dba2b94290d8a2d031f134e
suegoiee <suegoieenae@hotmail.com.tw>
2023-07-07 22:08:41 +0800

    only check timeout in each routine

--- main.go
+++ main.go
@@ -108,11 +108,14 @@ func init() {
 }
 
 func main() {
+	workerPoolSize := 10
+	workerPool := make(chan struct{}, workerPoolSize)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	url := "https://tw.stock.yahoo.com/news/"
-	getYahooNews(url, 1, ctx)
+	getYahooNews(url, 1, workerPool, ctx)
 
 	// Wait for the specified timeout duration
 	select {
@@ -121,184 +124,173 @@ func main() {
 	}
 }
 
-func getYahooNews(url string, lap int, ctx context.Context) {
-	// Check if the context has been canceled
-	select {
-	case <-ctx.Done():
-		// Context canceled, stop recursion
-		return
-	default:
-		if lap >= 3 {
-			return
-		}
-		lap += 1
-
-		res, err := http.Get(url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer res.Body.Close()
-
-		doc1, err := goquery.NewDocumentFromReader(res.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		results := make(map[string]bool)
-		doc1.Find("a").Each(func(i int, s *goquery.Selection) {
-			href, exists := s.Attr("href")
-			if exists {
-				linkExists := strings.Contains(href, "/news/")
-				domainExists := strings.Contains(href, "https://")
-				yahooExists := strings.Contains(href, "yahoo")
-				if linkExists && domainExists && yahooExists {
-					results[href] = true
-				} else if !domainExists && linkExists {
-					href = "https://tw.stock.yahoo.com" + href
-					results[href] = true
-				}
-			}
-		})
-
-		var wg sync.WaitGroup
-		newsList := make([]interface{}, 0)
-		wg.Add(len(results))
-
-		for link := range results {
-
-			filter := bson.M{
-				"link": link,
-			}
-
-			// Perform a query to check if the document already exists
-			count, err := coll.CountDocuments(context.TODO(), filter)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// If count is greater than zero, the document already exists
-			if count > 0 {
-				return
-			} else {
-				getYahooNews(link, lap, ctx)
-			}
-
-			go func(des string, ctx context.Context) {
-				// Check if the context has been canceled
-				select {
-				case <-ctx.Done():
-					// Context canceled, stop the goroutine
-					return
-				default:
-					defer wg.Done()
-					nextRes, err := http.Get(des)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					defer nextRes.Body.Close()
-					nextDoc, err := goquery.NewDocumentFromReader(nextRes.Body)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					var news YahooNews
-					nextDoc.Find("h1").Each(func(i int, s *goquery.Selection) {
-						newsTitle, exists := s.Attr("data-test-locator")
-						if newsTitle == "headline" && exists {
-							news.Title = s.Text()
-						}
-					})
-					news.Time = nextDoc.Find("time").Text()
-					nextDoc.Find("noscript").Remove()
-					var newsImages []NewsImage
-
-					// 提取圖片URL並下載
-					nextDoc.Find("div.caas-body img").Each(func(i int, s *goquery.Selection) {
-						imgSrc, exists := s.Attr("src")
-						if !exists {
-							imgSrc, exists = s.Attr("data-src")
-							if !exists {
-								return
-							}
-						}
-
-						s.AppendHtml("<p>{{img" + strconv.Itoa(i) + "}}<p>")
-						// Download the image from the URL
-						resp, err := http.Get(imgSrc)
-						if err != nil {
-							fmt.Println("Failed to download image:", err)
-							return
-						}
-						defer resp.Body.Close()
-
-						contentType := resp.Header.Get("Content-Type")
-						fileExtension := mimeToExtension(contentType)
-
-						// Get the filename from the URL
-						filename := ""
-						fileKey := ""
-						if strings.LastIndex(imgSrc, "/") != -1 {
-							filename = imgSrc[strings.LastIndex(imgSrc, "/")+1:] + fileExtension
-							fileKey = "images/" + filename
-						}
-
-						// Create a new S3 object
-						object := &s3.PutObjectInput{
-							Bucket: aws.String(s3Config.Bucket),
-							Key:    aws.String(fileKey),
-						}
-						buffer := bytes.NewBuffer(nil)
-
-						// Copy the response body into the buffer
-						_, err = io.Copy(buffer, resp.Body)
-						if err != nil {
-							fmt.Println("Failed to copy image data to buffer:", err)
-							return
-						}
-
-						// Set the object's body to the buffer
-						object.Body = bytes.NewReader(buffer.Bytes())
-
-						// Upload the image to S3
-						_, err = svc.PutObject(object)
-						if err != nil {
-							fmt.Println("Failed to upload image to S3:", err)
-							return
-						}
-
-						publicURL := fmt.Sprintf("https://%s.s3-%s.amazonaws.com/%s", s3Config.Bucket, s3Config.Region, fileKey)
-						figcaption := s.Closest("figure").Find("figcaption")
-						description := figcaption.Text()
-						figcaption.Remove()
-						newImage := NewsImage{
-							Link:        publicURL,
-							Description: description,
-						}
-						newsImages = append(newsImages, newImage)
-					})
-
-					news.Content = nextDoc.Find("div.caas-body").Text()
-					news.Images = newsImages
-					news.Source = "Yahoo"
-					news.Link = des
-					newsList = append(newsList, news)
-				}
-			}(link, ctx)
-		}
-		wg.Wait()
-
-		result, err := coll.InsertMany(context.TODO(), newsList)
-
-		if err != nil {
-			panic(err)
-		}
-
-		insertedCount := len(result.InsertedIDs)
-		fmt.Println("Inserted count:", insertedCount)
-
+func getYahooNews(url string, lap int, workerPool chan struct{}, ctx context.Context) {
+	if lap >= 3 {
 		return
 	}
+	lap += 1
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := make(map[string]bool)
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, exists := s.Attr("href")
+		if exists {
+			if linkFilter(href) {
+				results[href] = true
+			}
+		}
+	})
+
+	var wg sync.WaitGroup
+	newsList := make([]interface{}, 0)
+
+	for link := range results {
+		// Acquire a worker slot from the worker pool
+		workerPool <- struct{}{}
+		wg.Add(1)
+
+		filter := bson.M{
+			"link": link,
+		}
+
+		// Perform a query to check if the document already exists
+		count, err := coll.CountDocuments(context.TODO(), filter)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// If count is greater than zero, the document already exists
+		if count > 0 {
+			return
+		} else {
+			getYahooNews(link, lap, workerPool, ctx)
+		}
+
+		go func(des string, ctx context.Context) {
+			// Check if the context has been canceled
+			select {
+			case <-ctx.Done():
+				// Context canceled, stop the goroutine
+				return
+			default:
+				defer wg.Done()
+				nextRes, err := http.Get(des)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				defer nextRes.Body.Close()
+				nextDoc, err := goquery.NewDocumentFromReader(nextRes.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				var news YahooNews
+				nextDoc.Find("h1").Each(func(i int, s *goquery.Selection) {
+					newsTitle, exists := s.Attr("data-test-locator")
+					if newsTitle == "headline" && exists {
+						news.Title = s.Text()
+					}
+				})
+				news.Time = nextDoc.Find("time").Text()
+				nextDoc.Find("noscript").Remove()
+				var newsImages []NewsImage
+
+				// 提取圖片URL並下載
+				nextDoc.Find("div.caas-body img").Each(func(i int, s *goquery.Selection) {
+					imgSrc, exists := s.Attr("src")
+					if !exists {
+						imgSrc, exists = s.Attr("data-src")
+						if !exists {
+							return
+						}
+					}
+
+					s.AppendHtml("<p>{{img" + strconv.Itoa(i) + "}}<p>")
+					// Download the image from the URL
+					resp, err := http.Get(imgSrc)
+					if err != nil {
+						fmt.Println("Failed to download image:", err)
+						return
+					}
+					defer resp.Body.Close()
+
+					contentType := resp.Header.Get("Content-Type")
+					fileExtension := mimeToExtension(contentType)
+
+					// Get the filename from the URL
+					filename := ""
+					fileKey := ""
+					if strings.LastIndex(imgSrc, "/") != -1 {
+						filename = imgSrc[strings.LastIndex(imgSrc, "/")+1:] + fileExtension
+						fileKey = "images/" + filename
+					}
+
+					// Create a new S3 object
+					object := &s3.PutObjectInput{
+						Bucket: aws.String(s3Config.Bucket),
+						Key:    aws.String(fileKey),
+					}
+					buffer := bytes.NewBuffer(nil)
+
+					// Copy the response body into the buffer
+					_, err = io.Copy(buffer, resp.Body)
+					if err != nil {
+						fmt.Println("Failed to copy image data to buffer:", err)
+						return
+					}
+
+					// Set the object's body to the buffer
+					object.Body = bytes.NewReader(buffer.Bytes())
+
+					// Upload the image to S3
+					_, err = svc.PutObject(object)
+					if err != nil {
+						fmt.Println("Failed to upload image to S3:", err)
+						return
+					}
+
+					publicURL := fmt.Sprintf("https://%s.s3-%s.amazonaws.com/%s", s3Config.Bucket, s3Config.Region, fileKey)
+					figcaption := s.Closest("figure").Find("figcaption")
+					description := figcaption.Text()
+					figcaption.Remove()
+					newImage := NewsImage{
+						Link:        publicURL,
+						Description: description,
+					}
+					newsImages = append(newsImages, newImage)
+				})
+
+				news.Content = nextDoc.Find("div.caas-body").Text()
+				news.Images = newsImages
+				news.Source = "Yahoo"
+				news.Link = des
+				newsList = append(newsList, news)
+			}
+		}(link, ctx)
+	}
+	wg.Wait()
+
+	result, err := coll.InsertMany(context.TODO(), newsList)
+
+	if err != nil {
+		panic(err)
+	}
+
+	insertedCount := len(result.InsertedIDs)
+	fmt.Println("Inserted count:", insertedCount)
+
+	return
 }
 
 func mimeToExtension(contentType string) string {
@@ -312,4 +304,20 @@ func mimeToExtension(contentType string) string {
 	default:
 		return ""
 	}
+}
+
+func linkFilter(href string) bool {
+	// Check if the link is a news link based on specific criteria
+	linkExists := strings.Contains(href, "/news/")
+	domainExists := strings.Contains(href, "https://")
+	yahooExists := strings.Contains(href, "yahoo")
+
+	// Filter out unnecessary content by defining your criteria
+	if linkExists && domainExists && yahooExists {
+		return true
+	} else if !domainExists && linkExists {
+		return true
+	}
+
+	return false
 }
